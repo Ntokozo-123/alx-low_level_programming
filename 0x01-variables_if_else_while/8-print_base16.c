d646b0973b065f25abb073893e305eed14e4e1b4
Ntokozo-123 <gladness.manana@yahoo.com>
2022-12-11 02:50:43 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: prints all single digit numbers of base 10
+ * Description: prints all single digit numbers of base 16
  * starting from 0, followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	char c = 'z';	
+	char c = 'z';
+
 	while (c >= 'a')
 	{
 		putchar(c);
