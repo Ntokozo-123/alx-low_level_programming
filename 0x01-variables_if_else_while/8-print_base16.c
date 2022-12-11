cfb9e215bd47b1e7aef5b72714407fba6dd2041b
Ntokozo-123 <gladness.manana@yahoo.com>
2022-12-11 03:56:53 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 	char alpha;
 
 	for (num = 0; num < 10; num++)
-		putchar(c);
+		putchar(num % 10) + ('0');
 	for (alpha = 'a'; alpha <= 'f'; alpha++)
 		putchar(alpha);
 	putchar('\n');
