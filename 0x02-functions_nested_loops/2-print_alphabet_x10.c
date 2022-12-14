2743621c693d93b5f511de10ff50774954d327ba
Ntokozo-123 <gladness.manana@yahoo.com>
2022-12-14 10:20:04 -0800

    Alphabet game

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,12 @@
 
 void print_alphabet_x10(void)
 {
+	char a = 'a';
 
-	int count = 0;
-	char letter;
-
-	while (count++ <= 9)
+	for (a = 'a'; a <= 'z'; a++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter)
-
-		_putchar('/n')
-	}
+		_putchar(a);
+	}	
+	_putchar('\n');
 
 }
