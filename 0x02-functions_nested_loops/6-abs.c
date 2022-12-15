151deb92a4349dd4e1d4f6dd7560ae605a4a3c5e
Ntokozo-123 <gladness.manana@yahoo.com>
2022-12-15 01:05:20 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - prints the abs value of an int
+ * @n: the int in question
+ * Return: the absolute value of an int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
