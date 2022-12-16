968e121e6377d9af40bf8b71f4007f72bbe9f384
Ntokozo-123 <gladness.manana@yahoo.com>
2022-12-15 23:47:14 -0800

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,16 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-	{
+
 		_putchar('\n');
-	}
-	else
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 1; i <= n; i++)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 1; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('_');
+			_putchar(' ');
 		}
+
 		_putchar('\\');
 		_putchar('\n');
 	}
