7f760d016cec786853254bf8a42f7802e2dfa6f5
Ntokozo-123 <gladness.manana@yahoo.com>
2022-12-16 16:47:59 -0800

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,27 +3,26 @@
 /**
  * print_diagonal - a function that prints a diagonal line
  * @n: input number of times a diagonal line should be printed
- * Return: void
  */
-
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(32);
-			}
+		int i, j;
 
-			_putchar(92);
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
