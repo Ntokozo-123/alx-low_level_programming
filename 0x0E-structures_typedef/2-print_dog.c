d5d9e76a554050a11a580996dc13f1d92565873c
Ntokozo-123 <gladness.manana@yahoo.com>
2023-01-16 12:25:21 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		d->name = "(nil)";
+	if (d->owner == NULL)
 	if (d->owner = "(nil)";
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->
-age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
