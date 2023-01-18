aea28e7dfa9b8d06ce95d3a78c856123f46261fa
Ntokozo-123 <gladness.manana@yahoo.com>
2023-01-18 09:28:19 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "funcion_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
